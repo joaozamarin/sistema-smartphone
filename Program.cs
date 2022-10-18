8cb9df2a01976687599590c8080bb1fe137a6187
joaozamarin <joao.zamarin@gmail.com>
2022-10-18 14:50:17 -0300

    desafio DIO

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia("12345", "1º Geração", "0101010", 20);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Iphone iphone = new Iphone("98765", "11", "0909009", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
